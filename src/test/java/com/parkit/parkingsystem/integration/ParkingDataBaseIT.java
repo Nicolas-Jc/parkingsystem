e0ec4ccf9beeebf72cfddcede93f293d466e1e24
Nicolas Moreau <nicolas.jc.moreau@outlook.com>
2022-04-04 18:36:34 +0200

    Tests Integration

--- src/test/java/com/parkit/parkingsystem/integration/ParkingDataBaseIT.java
+++ src/test/java/com/parkit/parkingsystem/integration/ParkingDataBaseIT.java
@@ -1,5 +1,6 @@
 package com.parkit.parkingsystem.integration;
 
+import com.parkit.parkingsystem.constants.ParkingType;
 import com.parkit.parkingsystem.dao.ParkingSpotDAO;
 import com.parkit.parkingsystem.dao.TicketDAO;
 import com.parkit.parkingsystem.integration.config.DataBaseTestConfig;
@@ -14,6 +15,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -42,8 +44,10 @@ public class ParkingDataBaseIT {
         when(inputReaderUtil.readSelection()).thenReturn(1);
         when(inputReaderUtil.readVehicleRegistrationNumber()).thenReturn("ABCDEF");
         // Suppression de la base Ticket avant chaque lancement de test
-        // La base de test Ticket est donc censée être vide AVANT chaque lancement de test
-        // La base parking est censée avoir tous les spots disponibles
+        // La base de test Ticket est donc vidée AVANT chaque lancement de test
+        // La base parking est censée avoir tous les spots remis disponibles
+        // APRES CHAQUE TEST DONC :
+        // Un enregistrement Ticket "ABCDEF" est créé et l'indicateur du parking Slot 1 topé "Non disponible"
         dataBasePrepareService.clearDataBaseEntries();
     }
 
@@ -54,22 +58,40 @@ public class ParkingDataBaseIT {
 
     @Test
     public void testParkingACar() {
-        // Le test consiste donc à vérifier, qu'après lancement du test :
-        // 1. qu'un enregistrement avec la plaque d'immatriculation "ABCDEF"
-        // est bien créé en base de Test "Ticket"
-        // 2. que la place de parking (enregistrement No1 type CAR) est bien topée "0" (non disponible)
-        ParkingService parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
-        parkingService.processIncomingVehicle();
         //TODO: check that a ticket is actualy saved in DB and Parking table is updated with availability
-
+        //
+        // Le test consiste à vérifier, qu'après lancement du test :
+        // 1. un enregistrement avec la plaque d'immatriculation "ABCDEF"
+        // est bien créé en base de Test "Ticket"
+        // 2. que la place de parking (enregistrement No2 type CAR) est bien topée "1" (disponible)
+        // GIVEN
+        ParkingService parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
+        // WHEN
+        parkingService.processIncomingVehicle();
+        // THEN
+        assertThat(ticketDAO.getTicket("ABCDEF")).isNotNull();
+        // Le 1er Spot de Parking disponible est le deuxième (le premier étant pris pour "ABCDEF")
+        assertThat(parkingSpotDAO.getNextAvailableSlot(ParkingType.CAR)).isEqualTo(2);
     }
 
+    /*
     @Test
     public void testParkingLotExit() {
-        testParkingACar();
-        ParkingService parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
-        parkingService.processExitingVehicle();
         //TODO: check that the fare generated and out time are populated correctly in the database
-    }
+        //
+        // GIVEN
+        testParkingACar();
+        // Un Ticket "ABCDEF" est bien enregistré en base et le 1er Spot Parking est non disponible
+        //when(inputReaderUtil.readSelection()).thenReturn(1);
+        // when(inputReaderUtil.readVehicleRegistrationNumber()).thenReturn("ABCDEF");
+        ParkingService parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
+        // Ecrire un Ticket "SaveTicket" - Avoir un ticket en base
+        //
+        // WHEN
+        parkingService.processExitingVehicle();
+        // THEN
+        assertThat(ticketDAO.)
+
+    }*/
 
 }
